cf686c81dc781e08edf3dbf0d80f3f6ff7379b8d
JulianISGON <julian.isaza1020@gmail.com>
2023-02-07 20:01:18 -0500

    push

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -10,11 +10,11 @@ import { HttpClient } from '@angular/common/http';
 })
 
 export class MainComponent {
+  cursos:Array<any> = []
   constructor(private http: HttpClient) { }
   url = "http://localhost:3000/cursos";
   async getConditionalDataUsingAsync() {
-    let data = await this.http.get<any>(this.url).toPromise();
-    console.log(data)
+    this.cursos = await this.http.get<any>(this.url).toPromise();
   }
   ngOnInit() {
     this.getConditionalDataUsingAsync()
