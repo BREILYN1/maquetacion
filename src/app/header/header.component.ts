b8df1ef06c2202954dd1fae8cdeee71ff0f0b275
Breylin Stiven Velasquez Hernández <breylin.velasquez@cadena.com.co>
2023-02-15 20:49:15 -0500

    nuevos cambios

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+
+  fecha = new Date()
+  dia = this.fecha.getDay()
+  frase = ""
+
+  frases:Array<any> = []
+  constructor(private http: HttpClient) { }
+  url = "http://localhost:3004/frases";
+  async getConditionalDataUsingAsync() {
+    this.frases = await this.http.get<any>(this.url).toPromise();
+    this.frase = JSON.stringify(this.frases[this.dia -1].frase);
+  }
+  ngOnInit() {
+    this.getConditionalDataUsingAsync()
+  }
+
+  
 
 }
